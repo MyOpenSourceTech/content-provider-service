e01609d71e4ee772f175aba1cb7539a80d25f94b
Sourav Kabiraj <sourav.kabiraj@outlook.com>
2020-08-01 20:57:57 +0530

    #001 | Sourav | initial setup of content provider service

---
+++ src/main/java/com/coffeebrew/contentproviderservice/ContentProviderServiceApplication.java
@@ -0,0 +1,13 @@
+package com.coffeebrew.contentproviderservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContentProviderServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContentProviderServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/coffeebrew/contentproviderservice/ContentProviderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coffeebrew.contentproviderservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContentProviderServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
