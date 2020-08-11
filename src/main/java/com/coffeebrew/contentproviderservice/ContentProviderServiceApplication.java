641e5bf047646d03ae9db0fa65ea2da8862df2d4
Sourav Kabiraj <sourav.kabiraj@outlook.com>
2020-08-11 22:58:09 +0530

    #006 | Sourav | Minor change.

--- src/main/java/com/coffeebrew/contentproviderservice/ContentProviderServiceApplication.java
+++ src/main/java/com/coffeebrew/contentproviderservice/ContentProviderServiceApplication.java
@@ -4,8 +4,10 @@ import com.coffeebrew.contentproviderservice.configs.FileStorageProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 @EnableConfigurationProperties({
         FileStorageProperties.class
 })
--- src/main/java/com/coffeebrew/contentproviderservice/repositories/ContentRepository.java
+++ src/main/java/com/coffeebrew/contentproviderservice/repositories/ContentRepository.java
@@ -2,6 +2,8 @@ package com.coffeebrew.contentproviderservice.repositories;
 
 import com.coffeebrew.contentproviderservice.models.Content;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ContentRepository extends CrudRepository<Content, String> {
 }
